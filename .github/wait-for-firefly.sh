54b1fbb571721a1d068c7f420f05c5edaaabb797
vlad1kek <streelok10@gmail.com>
2024-02-25 00:31:25 +0200

    First

---
+++ .github/wait-for-firefly.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+times=15
+while ! curl -sSL 'http://localhost:8080/register' 2>&1 \
+             | grep '<html' >/dev/null; do
+    echo 'Waiting for the Firefly'
+    sleep 10
+    times=$(($times - 1))
+
+    if [ $times -le 0 ]; then
+        echo 'Time out'
+        exit 1
+    fi
+done
+
+echo 'The Firefly is up'
