062d3d1d390b35a70b185e393850500f3e71412c
vlad1kek <streelok10@gmail.com>
2024-02-25 23:56:54 +0200

    First

--- src/test/java/utils/run/ProjectProperties.java
+++ src/test/java/utils/run/ProjectProperties.java
@@ -63,11 +63,12 @@ public final class ProjectProperties {
         initProperties();
 
         chromeOptions = new ChromeOptions();
-        String options = properties.getProperty(PROP_CHROME_OPTIONS);
+        String options = System.getProperty(PROP_CHROME_OPTIONS);
+        if (options == null) {
+            options = properties.getProperty(PROP_CHROME_OPTIONS);
+        }
         if (options != null) {
-            for (String argument : options.split(";")) {
-                chromeOptions.addArguments(argument);
-            }
+            chromeOptions.addArguments(options.split(";"));
         }
 
         WebDriverManager.chromedriver().setup();
