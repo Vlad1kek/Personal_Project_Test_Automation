5f5ac11bd1a8264acea0b514893f8d038090f972
vlad1kek <streelok10@gmail.com>
2024-03-05 01:30:08 +0200

    FIx

---
+++ src/test/java/page/Budgets/BudgetsTransactionsPage.java
@@ -0,0 +1,20 @@
+package page.Budgets;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import page.Base.BasePage;
+
+public class BudgetsTransactionsPage extends BasePage {
+
+    @FindBy(className = "money-positive")
+    private WebElement amountText;
+
+    public BudgetsTransactionsPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public String getAmountText() {
+        return amountText.getText();
+    }
+}
